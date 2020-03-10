4ab17d4d95ed6ce74a5c13cafef756cce1b66742
Andy Pak <ap@Andys-MacBook-Pro.local>
2020-03-10 14:00:12 -0400

    Refactor internal variables in Board class to be 'private' and add max round number so games don't go on for too long.

--- helper/board.py
+++ helper/board.py
@@ -8,49 +8,48 @@ from .player import Player
 
 class Board:
     def __init__(self, deck=Deck().build(), num_players=NUM_PLAYERS):
-        self.game_deck = deck
-        self.game_deck.shuffle()
-        self.players = []
+        self._game_deck = deck
+        self._game_deck.shuffle()
+        self._players = []
         self._create_players(num_players)
 
         self._round_cards = []
         self._round_spoils = []
 
         print('PRINTING PLAYERS')
-        for p in self.players:
+        for p in self._players:
             print(p.name, p.deck.size())
     
     def _create_players(self, num_players):
-        num_player_cards = int(self.game_deck.size() / num_players)
+        num_player_cards = int(self._game_deck.size() / num_players)
         for i in range(0, num_players):
             start = i * num_player_cards
             end = (i+1) * num_player_cards
 
-            player_deck = Deck(**{'cards': self.game_deck.cards[start:end]})
-            self.players.append(Player('P{}'.format(i+1), player_deck))
+            player_deck = Deck(**{'cards': self._game_deck.cards[start:end]})
+            self._players.append(Player('P{}'.format(i+1), player_deck))
 
     def _continue_game(self):
         cards_in_play = len(self._round_cards) + len(self._round_spoils)
-        for p in self.players:
+        for p in self._players:
             if p.spoils.size() + len(p.deck.cards) + cards_in_play == NUM_CARDS_IN_DECK * NUM_DECKS:
                 print('Player: ' + p.name + ' is the winner!')
                 return False
         return True
 
-    # used for combining cards
+    # Convenience function used for aggregating cards
     def _get_round_cards(self, round_cards):
         return [rc[1] for rc in round_cards]
 
     def _get_players_cards(self, is_war):
         if is_war:
             print('----------THIS IS WAR----------')
-            for p in self.players:
+            for p in self._players:
                 for i in range(0, MILL_COUNT):
                     draw_card = p.draw_card()
 
                     if draw_card == -1:
-                        self._continue_game()
-                        return False
+                        return self._continue_game()
                     # Last card drawn, put card back in deck to use as round card
                     # elif p.deck.size() == 0:
                     #     p.deck.cards.append(draw_card)
@@ -59,28 +58,27 @@ class Board:
                         self._round_spoils.append(draw_card)
 
         # Add cards to card pool to be evaluated
-        for p in self.players:
+        for p in self._players:
             draw_card = p.draw_card()
 
             if draw_card == -1:
-                self._continue_game()
-                print(p.name, ' couldn\'t draw?')
-                return False
+                return self._continue_game()
             else:
                 self._round_cards.append((p, draw_card))
 
         print('FACEOFF')
-        print('round_cards: ', len(self._round_cards), 'round_spoils: ', len(self._round_spoils))
+        player_card_count = 0
         for rc in self._round_cards:
-            print(rc[0].name, rc[1].suit.value, rc[1].value.value, 'deck: {}'.format(rc[0].deck.size()), 'spoils: {}'.format(rc[0].spoils.size()))
-
+            player_card_count += rc[0].deck.size() + rc[0].spoils.size()
+            print(rc[0].name, rc[1].suit.value, rc[1].value.value, 'deck: ', rc[0].deck.size(), 'spoils: ', rc[0].spoils.size())
+        print('round_cards: ', len(self._round_cards), 'round_spoils: ', len(self._round_spoils), 'total_cards: ', len(self._round_cards) + len(self._round_spoils) + player_card_count)
         return True
 
     # sorted_cards = [(Player, Card), ...]
     def _check_dups(self, sorted_cards):
         highest = sorted_cards[0][1].value.value
         dups = [sorted_cards[0]]
-        for i in range(1, len(self.players)):
+        for i in range(1, len(self._players)):
             if sorted_cards[i][1].value.value == highest:
                 dups.append(sorted_cards[i])
         return dups
@@ -115,6 +113,9 @@ class Board:
         play = True
         count = 1
         while play:
+            if count > MAX_ROUND:
+                print('---------- MAX ROUND REACHED, GAME IS A TIE!!! ----------')
+                break
             print('ROUND: ', count)
             play = self.play_round(False)
             count += 1
--- helper/config.py
+++ helper/config.py
@@ -6,3 +6,5 @@ NUM_DECKS = 1
 NUM_CARDS_IN_DECK = len(Suit) * len(Value)
 # Number of cards removed from top of deck for each player during "War"
 MILL_COUNT = 3
+# Max number of rounds in the case the game enters into a cyclic pattern and never ends
+MAX_ROUND = 1000
